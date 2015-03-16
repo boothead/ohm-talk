98c6844640d9a37db7956e86d78c618353b94b27
Ben Ford <ben@perurbis.com>
2015-03-16 17:52:10 +0000

    Move the slides out to the Deck module

--- client/src/Main.hs
+++ client/src/Main.hs
@@ -6,50 +6,19 @@
 
 module Main where
 
-import qualified Data.Map      as Map
-import           Data.String   (fromString)
-import qualified Data.Text     as T
+
 import           MVC
 import           Ohm.Component
 import           Ohm.HTML
 import           Ohm.KeyMaster
 import           Prelude       hiding (div, filter, id, map, span)
-import           Present
-import           Present.Types
 import           Present.Model
+import           Present.Types
+
+import           Deck
 import           Slide
 
 
--- addClass p (i, s) = (s', rect)
---   where s' = s &~ (classes .= [c])
---         rect = Rectangle 0 0 0 0
-
-introSection :: [SC SModel Edom ]
-introSection = mdExtSlide : mdSlide : [Slide (T.pack $ show t) (slideText t) Nothing "" | t <- [(1::Int)..5]]
-  where slideText i = Plain $ into h2_ [fromString $ "intro " ++ show i]
-        mdExtSlide = Slide ("Markdown") (MDFile "mdtest.md") Nothing "A Note"
-        mdSlide = Slide "Inline" (MD md) Nothing "Inline note"
-        md = T.pack $ unlines [
-                 "# Inline test"
-               , ""
-               , "Blah"
-               ]
-
-intro :: SlideSpace SModel Edom
-intro = Workspace "intro" (Layout $ SlideLayout 900 600 V) (differentiate introSection)
-
-
-problemSection :: [SC SModel Edom]
-problemSection = [Slide (T.pack $ show t) (slideText t) Nothing "" | t <- [(1::Int)..5]]
-  where slideText i = Plain $ into h2_ [fromString $ "problem " ++ show i]
-
-problem :: SlideSpace SModel Edom
-problem = Workspace "problem" (Layout $ SlideLayout 900 600 V) (differentiate problemSection)
-
-ss :: SlideState SModel Edom
-ss = StackSet (scrn intro) [] [problem] Map.empty
-  where scrn slides' = Screen slides' (S 0) (SD $ Rectangle 0 0 900 600)
-  
 foreign import javascript unsafe
   "RevealMarkdown.initialize()"
   convertMDSlides :: IO ()
@@ -68,6 +37,6 @@ main = do
     , ("1", ToSection "intro")
     , ("2", ToSection "problem")
     ]
-  modelSink <- runComponent (AppState ss SModel) () slideComponent
+  modelSink <- runComponent (AppState deck SModel) () slideComponent
   forkProcessor () $ for (fromInput keySource) (runProcessor $ domEventsProcessor slideComponent)
                >-> (toOutput modelSink)
